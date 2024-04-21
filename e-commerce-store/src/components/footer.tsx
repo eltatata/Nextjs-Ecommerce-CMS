107b93d251e57d8cc6bcd5fcdf1a13f04c518945
eltatataa <davidtowo75@gmail.com>
2024-04-21 00:02:52 -0500

    add store

---
+++ e-commerce-store/src/components/footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer className='bg-white border-l'>
+      <div className='mx-auto py-10'>
+        <p className='text-center text-xs text-black'>
+          &copy; 2024 FakeStoreName, Inc. All rights reserved
+        </p>
+      </div>
+    </footer>
+  )
+}
