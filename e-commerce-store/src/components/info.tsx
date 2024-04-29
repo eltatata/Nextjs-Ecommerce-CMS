d7300c30a97b93e864ab92cdb990ef2a75324cb0
eltatataa <davidtowo75@gmail.com>
2024-04-29 08:41:56 -0500

    button to add to cart fixed

--- e-commerce-store/src/components/info.tsx
+++ e-commerce-store/src/components/info.tsx
@@ -6,12 +6,19 @@ import { ShoppingCart } from "lucide-react"
 
 import Currency from "@/components/ui/currency"
 import Button from "@/components/ui/button"
+import useCart from "@/hooks/use-cart"
 
 interface InfoProps {
   data: Product
 }
 
 export default function Info({ data }: InfoProps) {
+  const cart = useCart();
+
+  const onAddToCart = () => {
+    cart.addItem(data)
+  }
+
   return (
     <div>
       <h1 className="text-3xl font-bold text-gray-900">{data.name}</h1>
@@ -37,7 +44,7 @@ export default function Info({ data }: InfoProps) {
         </div>
       </div>
       <div className="mt-10 flex items-center gap-x-3">
-        <Button className="flex items-center gap-x-2">
+        <Button onClick={onAddToCart} className="flex items-center gap-x-2">
           Add To Cart
           <ShoppingCart className="" />
         </Button>
